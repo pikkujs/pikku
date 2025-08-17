af95a5901b16d2976731a17693fcc1a538fce381
Yasser Fadl <yasser.fadl@gmail.com>
2025-08-17 11:15:47 +0200

    fix: adding a number type (string or number) to db for now. Going forward should probably parse to a number but js and floats..

---
+++ .changeset/empty-jokes-refuse.md
@@ -0,0 +1,5 @@
+---
+'@pikku/kysely': patch
+---
+
+fix: adding a number type (string or number) to db for now. Going forward should probably parse to a number but js and floats..
--- packages/services/kysely/bin/pikku-kysely-pure.ts
+++ packages/services/kysely/bin/pikku-kysely-pure.ts
@@ -23,12 +23,16 @@ const main = async (output: string, input?: string) => {
   }
 
   kysely = kysely
+    .replace('import type { ColumnType } from "kysely";', '')
     .replace(
       new RegExp('^export\\s+type\\s+Generated<[^>]+>.*?;\\s*$', 'ms'),
       ''
     )
     .replace(/Generated<(.*)>/g, '$1')
-    .replace(/export type Numeric =.*;/, 'string | number')
+    .replace(
+      /export type Numeric =.*;/,
+      'export type Numeric = string | number'
+    )
     .replace(/export type Timestamp =.*;/, 'export type Timestamp = Date')
 
   console.log('Writing kysely-pure.gen.ts')
