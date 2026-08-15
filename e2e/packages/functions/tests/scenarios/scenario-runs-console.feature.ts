7de8732c42941c364889748d6bad0570b8199ce4
Yasser Fadl <yasser.fadl@gmail.com>
2026-08-15 23:54:48 +0200

    fix(e2e): import the new runs feature from the scenario barrel

--- e2e/packages/functions/tests/scenarios/scenario-runs-console.feature.ts
+++ e2e/packages/functions/tests/scenarios/scenario-runs-console.feature.ts
@@ -16,7 +16,7 @@
 import {
   pikkuFeature,
   pikkuScenario,
-} from '#pikku/workflow/pikku-workflow-types.gen.js'
+} from '#pikku/scenarios/pikku-scenario-types.gen.js'
 
 const RUNS_PAGE = '/console/scenarios?view=runs'
 
